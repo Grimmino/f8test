71d86e833fff9f0b7ab02f9ef02fcec8fa4dd3f4
Sowa <Grimmino@gmail.com>
2019-12-30 11:58:14 +0500

    init

---
+++ static/index.js
@@ -0,0 +1,5 @@
+//styles
+import 'normalize.css'
+import './sass/style.sass'
+//js
+import './js/main'
---
+++ static/js/main.js
@@ -0,0 +1,71 @@
+//show/hide popup
+let modal = document.querySelector('.modal')
+let modalToggler = document.querySelector('.btn__modal')
+let closeModal = document.querySelector('.modal__close')
+
+modalToggler.addEventListener('click', function() {
+    modal.classList.toggle('modal--show')
+})
+
+function hideModal() {
+    modal.classList.remove('modal--show')
+}
+
+closeModal.addEventListener('click', function() {
+    hideModal()
+})
+
+
+//form
+let formElem = document.querySelector('.form')
+let formLabel = formElem.querySelectorAll('.form__label:not(.form__label_checkbox)')
+
+//form-dropdown
+let formDropDownItem = formElem.querySelectorAll('.form__dropdown__item')
+
+formDropDownItem.forEach(item => {
+    item.addEventListener('click', function() {
+        console.log(123)
+        let input = item.closest('.form__label').querySelector('.form__input')
+        input.value = item.textContent
+        input.classList.add('form__input--focus')
+    })
+})
+
+//input focus/not empty
+formLabel.forEach((item) => {
+    item.querySelector("input").addEventListener("input", function(e) {
+        e.target.value != '' ? e.target.classList.add('form__input--focus') : e.target.classList.remove('form__input--focus')
+    })
+})
+
+function inputValid(item) {
+    item.classList.contains('error') ? item.classList.remove('error') : null
+    item.classList.add('valid')
+}
+function inputInvalid(item) {
+    item.classList.contains('valid') ? item.classList.remove('valid') : null
+    item.classList.add('error')
+}
+
+//form validation
+formElem.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    formLabel.forEach(item => {
+
+        let input = item.querySelector("input")
+
+        switch(input.type) {
+            case "text":
+            case "date":
+                input.value != '' ? (inputValid(item),false) : inputInvalid(item)
+                break
+            case 'email':
+                input.value != '' && /\w*@\w*\.\w*/.test(input.value) ? (inputValid(item),false) : inputInvalid(item)
+                break
+        }
+    })
+
+    return false
+})
